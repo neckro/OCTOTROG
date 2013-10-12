f89bddf0ab10c92ca4a17121247ccc826ec79c96
Joseph C <neckro@gmail.com>
2013-10-11 22:31:59 -0500

    Add !team command for tourney

--- octotrog.js
+++ octotrog.js
@@ -1,6 +1,7 @@
 "use strict";
 var irc = require('irc');
 var relaybot = require('./relaybot');
+var sprintf = require('sprintf').sprintf;
 
 var bot_options = {
   main_server: "irc.lunarnet.org",
@@ -18,7 +19,7 @@ var bot_options = {
   savefile: './octosave.json',
   command_regex: /^[!$?%@]/,
   sayings: {
-    tourney_info: "Tournament starts on Oct 11, 2013 at 20:00 UTC, and ends on Oct 27, 2013 at 20:00 UTC. More info: http://dobrazupa.org/tournament/0.13/",
+    tourney_info: "Tournament ends on Oct 27, 2013 at 20:00 UTC. http://dobrazupa.org/tournament/0.13/overview.html",
     greeting: "kill them all?",
     kicked: irc.colors.wrap('light_red', 'trog vigorously angry.'),
     help: "my commands are: %s. praise be to trog.",
@@ -42,8 +43,42 @@ var bot_options = {
           response: function(opt) {
             this.bot.main_client.say(opt.reply, this.bot.sayings.tourney_info);
           }
+        },
+        "team": {
+          description: "Show your team info, if you're on one.",
+          response: function(opt) {
+            var nick = opt.nick;
+            var teams = this.bot.saved.teams;
+            var team_name;
+            if (!teams) return;
+            if (typeof opt.params[0] === 'string') nick = opt.params[0];
+            Object.keys(teams).some(function(name) {
+              var members = teams[name];
+              if (!Array.isArray(members)) return;
+              if (members.indexOf(nick.toLowerCase()) !== -1) {
+                team_name = name;
+                return true;
+              }
+            });
+            if (team_name) {
+              this.bot.main_client.say(opt.reply, sprintf(
+                'PLAYER %s ON TEAM: %s http://dobrazupa.org/tournament/0.13/clans/%s.html http://dobrazupa.org/tournament/0.13/players/%s.html',
+                nick.toUpperCase(),
+                team_name,
+                teams[team_name][0].toLowerCase(),
+                nick.toLowerCase()
+              ));
+              this.bot.say_text(opt.reply,
+                'members: %s (captain), %s',
+                teams[team_name].shift(),
+                teams[team_name].join(', ')
+              );
+            } else {
+              this.bot.say_text(opt.reply, 'player %s is not on a team.', nick);
+            }
+          }
         }
-      }
+      },
     }
   ],
   say_transform: function(text) {
