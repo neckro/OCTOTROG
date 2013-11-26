4a584786df512641d2e5b061d54cdb1507fa1f03
Joseph C <neckro@gmail.com>
2013-11-26 13:59:12 -0600

    Fix challenge date

--- www/index.php
+++ www/index.php
@@ -219,7 +219,7 @@ if (!empty($_GET['player'])) {
 } else {
   echo TrogReport::listPlayers();
   echo TrogReport::showHistory(10);
-  echo TrogReport::showCombo('Hu', 'Wn', '2013-11-25', '2013-12-02');
+  echo TrogReport::showCombo('Hu', 'Wn', '2013-11-25', '2013-12-01');
   echo TrogReport::showCombo('Gh', 'EE', '2013-11-18', '2013-11-24');
 }
 ?>
