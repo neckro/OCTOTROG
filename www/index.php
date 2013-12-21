32eb8eb298d85c5e147d46db910c38fe5ace1146
Aaron Wilson <aaron@olark.com>
2013-12-21 13:29:58 -0500

    Add DEWz and KoAs

--- www/index.php
+++ www/index.php
@@ -219,6 +219,8 @@ if (!empty($_GET['player'])) {
 } else {
   echo TrogReport::listPlayers();
   echo TrogReport::showHistory(10);
+  echo TrogReport::showCombo('Ko', 'As', '2013-12-16', '2013-12-28');
+  echo TrogReport::showCombo('DE', 'Wz', '2013-12-09', '2013-12-15');
   echo TrogReport::showCombo('Mi', 'Fi', '2013-12-02', '2013-12-08');
   echo TrogReport::showCombo('Hu', 'Wn', '2013-11-25', '2013-12-01');
   echo TrogReport::showCombo('Gh', 'EE', '2013-11-18', '2013-11-24');
